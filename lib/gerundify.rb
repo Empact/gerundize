c90b2e6caa339cca8957efa7fa44b616f4e930c7
Ben Woosley <ben.woosley@gmail.com>
2009-07-27 14:29:49 -0400

    Generalize doubles

--- lib/gerundify.rb
+++ lib/gerundify.rb
@@ -1,12 +1,18 @@
 module Gerundify
   GERUND_EXCEPTIONS = {
-    'blue' => 'blueing',
-
-    'brag' => 'bragging',
+    'blue' => 'blueing'
   }
 
-  GERUND_DOUBLE_TS = %w(submit bet blot chat cut dot fit flat flit forget get jot
-    knit knot shut sit slit slot spat spit unset wet wit)
+  GERUND_DOUBLES = %w(
+    brag plug
+
+    zip
+
+    submit bet blot chat cut dot fit flat flit forget get jot
+    knit knot shut sit slit slot spat spit unset wet wit plot
+
+    abhor bar concur demur jar scar spur star stir transfer unjar
+  )
 
   def gerundify
     GERUND_EXCEPTIONS[self] ||
@@ -16,8 +22,8 @@ module Gerundify
       "#{$1}ying"
     elsif self =~ /(.*)e$/
       "#{$1}ing"
-    elsif GERUND_DOUBLE_TS.include?(self.downcase)
-      "#{self}ting"
+    elsif GERUND_DOUBLES.include?(self.downcase)
+      "#{self + self[-1,1]}ing"
     else
       "#{self}ing"
     end
