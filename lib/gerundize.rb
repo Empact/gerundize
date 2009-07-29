3526a820a7090d655ad7b093b05f091179a57624
Ben Woosley <ben.woosley@gmail.com>
2009-07-29 00:04:45 -0400

    Add more doubles

--- lib/gerundize.rb
+++ lib/gerundize.rb
@@ -1,17 +1,39 @@
 module Gerundize
   GERUND_EXCEPTIONS = {
-    'blue' => 'blueing'
+    'hoe' => 'hoeing',
+    'pee' => 'peeing',
+    'pie' => 'pieing',
+    'dye' => 'dyeing',
+    'see' => 'seeing',
+    'free' => 'freeing',
+    'emcee' => 'emceeing',
+    'cue' => 'cueing'
   }
 
   GERUND_DOUBLES = %w(
-    brag plug
+    rub snub
 
-    zip
+    bid shed outbid
 
-    submit bet blot chat cut dot fit flat flit forget get jot
-    knit knot shut sit slit slot spat spit unset wet wit plot
+    snug bag hog
 
-    abhor bar concur demur jar scar spur star stir transfer unjar
+    brag plug hag smug snag snap snip
+
+    yodel fuel expel total dispel
+
+    hum clam ham shim cram
+
+    gin dun sin shun shin
+
+    up zip clap mop sap shop ship yap crap recap
+
+    blur slur abhor bar concur demur jar scar spur star stir transfer unjar recur
+
+    focus
+
+    submit bet blot chat cut dot fit flat flit forget get jot cat fret
+    knit knot shut sit slit slot spat spit unset wet wit plot nut shit
+    emit snot facet sublet tat hit
   )
 
   def gerundize
